da7e68dd30a3605312237145b77e5755fa909cf3
jainer <bizarrada@gmail.com>
2021-03-03 01:29:14 -0400

    feat: adding local in-memory database

--- index.js
+++ index.js
@@ -23,10 +23,40 @@ app.get("/", (_, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("New client connected", { socketID: socket.id });
+  console.log("New client connected", {
+    socketID: socket.id,
+    query: socket.handshake.query,
+  });
+
+  const user_id = socket.handshake.query.id;
+  const doc = {
+    socket_id: socket.id,
+    user_id,
+  };
+
+  db.insert(doc);
+
+  db.find({}, function (err, doc) {
+    // doc is the document Mars
+    // If no document is found, doc is null
+    console.log({ err, doc }, "todos");
+    io.emit("user-list", doc);
+  });
 
   socket.on("disconnect", () => {
     console.log("Client disconnected");
+
+    db.remove({ user_id }, {}, function (err, numRemoved) {
+      console.log({ numRemoved });
+      // numRemoved = 1
+    });
+
+    db.find({}, function (err, doc) {
+      // doc is the document Mars
+      // If no document is found, doc is null
+      console.log({ err, doc }, "todos");
+      io.emit("user-list", doc);
+    });
   });
 });
 
